690859096d89d89c59e294decabb0d7868a68819
Kiss László <lacus@kiss-air.home>
2020-09-28 22:44:38 +0200

    add AES encryption to front end

--- client/reciever.js
+++ client/reciever.js
@@ -1,7 +1,12 @@
+var CryptoJS = require("crypto-js");
+
 const socket = io.connect(window.location.origin);
 
-let text = document.getElementById('text-container');
+const text = document.getElementById('text-container');
+const SECRET_KEY = window.location.hash.substr(1);
 
 socket.on('typing', data => {
-  text.innerHTML =  '<p id="text">' + data + '</p>';
+  console.log(SECRET_KEY);
+  let message = CryptoJS.AES.decrypt(data, SECRET_KEY).toString(CryptoJS.enc.Utf8);
+  text.innerHTML = '<p id="text">' + message + '</p>';
 });
--- client/session.js
+++ client/session.js
@@ -1,15 +1,27 @@
+var CryptoJS = require("crypto-js");
+const { getSecretKey } =  require('./service');
 const socket = io.connect(window.location.origin);
 
-let url = document.getElementById('url');
-let message = document.getElementById('message');
-url.value = window.location.origin + '/reciever'
+const url = document.getElementById('url');
+const message = document.getElementById('message');
+const btn = document.getElementById('btn-copy');
+const SECRET_KEY = getSecretKey(12);
+window.location.hash = SECRET_KEY;
+console.log(window.location);
+url.value = window.location.origin + '/reciever' + window.location.hash;
 
-let copyUrl = () => {
+btn.addEventListener('click', () => {
   url.select();
   url.setSelectionRange(0, 99999);
-  document.execCommand("copy");
+  document.execCommand('copy');
+})
+
+const encryptMessage = (message) => {
+  return CryptoJS.AES.encrypt(message, SECRET_KEY).toString();
 }
 
 message.addEventListener('keypress', () => {
-  socket.emit('typing', message.value);
+  // BUG: last character appears only a new keypress
+  let encryptedMessage = encryptMessage(message.value);
+  socket.emit('typing', encryptedMessage);
 });
