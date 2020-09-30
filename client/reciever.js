285a92bc69de21ff03a470d556ce46351192e55a
Kiss László <lacus@kiss-air.home>
2020-09-30 11:04:03 +0200

    refactor front end

--- client/reciever.js
+++ client/reciever.js
@@ -3,13 +3,20 @@ const { decryptMessage, isDataUntouched } = require('./service');
 const socket = io.connect(window.location.origin);
 
 const text = document.getElementById('text-container');
+const warning = document.getElementById('warning');
 const SECRET_KEY = window.location.hash.substr(1);
+
+if (SECRET_KEY === "" || SECRET_KEY === undefined) {
+  warning.innerHTML = '<p class="warning"> You have no access to any chanel. </p>';
+  socket.close();
+}
 
 socket.on('typing', data => {
   const message = decryptMessage(data.encryptedMessage, SECRET_KEY);
   if (isDataUntouched(message, data.hmac, SECRET_KEY)) {
     text.innerHTML = '<p id="text">' + message + '</p>';
   } else {
-    text.innerHTML = '<p class="red"> This is a warning message! The chanel is not secure, please close it! </p>';
+    warning.innerHTML = '<p class="warning"> This is a warning message! The chanel is not secure, please close it! </p>';
+    socket.close();
   }
 });
